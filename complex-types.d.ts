9dc86cd56e8aa61527bfa40aef1677b4a85299ee
Titus Wormer <tituswormer@gmail.com>
2021-07-25 17:56:49 +0200

    Refactor to improve identifiers

--- complex-types.d.ts
+++ complex-types.d.ts
@@ -1,5 +1,10 @@
 import type {Node, Parent} from 'unist'
 
-export type NodeInTree<T extends Node, A = void> = T extends Parent
-  ? T | NodeInTree<Exclude<T['children'][number], A | T>, A | T>
-  : T
+export type NodeInTree<Tree extends Node, Found = void> = Tree extends Parent
+  ?
+      | Tree
+      | NodeInTree<
+          Exclude<Tree['children'][number], Found | Tree>,
+          Found | Tree
+        >
+  : Tree
