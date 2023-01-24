529f0648844409b99a3da9b1d7b526aa5be2a461
Titus Wormer <tituswormer@gmail.com>
2023-01-24 14:40:22 +0100

    Fix hidden types for `unist-util-visit`

--- complex-types.d.ts
+++ complex-types.d.ts
@@ -1,11 +1,14 @@
 // To do: next major: remove this file?
 export type {
   // Used in `unist-util-visit`:
-  Matches,
-  InclusiveDescendant,
   VisitorResult,
 
   // Documented:
   Visitor,
   BuildVisitor
 } from './index.js'
+export type {
+  // Used in `unist-util-visit`:
+  Matches,
+  InclusiveDescendant
+} from './lib/complex-types.js'
