973099e29cdee19a1efb7625f9d440464461bdbf
Titus Wormer <tituswormer@gmail.com>
2023-01-24 12:57:39 +0100

    Refactor to move implementation to `lib/`

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+export type {Test} from 'unist-util-is'
+export type {
+  Action,
+  ActionTuple,
+  Index,
+  VisitorResult,
+  Visitor,
+  BuildVisitor
+} from './lib/complex-types.js'
+export {CONTINUE, EXIT, SKIP, visitParents} from './lib/index.js'
